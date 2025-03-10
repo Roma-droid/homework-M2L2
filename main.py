82b42cc90fa28ece0c4a3290cb0feb32adb32bf1
Roma-droid <volkovromav@gmail.com>
2025-03-10 14:26:17 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot import TeleBot
+import lmstudio as lms
+bot = telebot.TeleBot("7294624873:AAEA6i-U03rMzCpVmgEtAeR6gagQLmaE0ew")
+
+
+@bot.message_handler(commands=['start', 'info'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to ChatGPT by nature, write a question")
+
+@bot.message_handler(commands=['about'])
+def send_adout(message):
+    model = lms.llm("llama-3.1-tulu-3.1-8b")
+    result = model.respond("Напиши о природе")
+    bot.reply_to(message, result)
+
+@bot.message_handler(commands=['news'])
+def send_news(message):
+    model = lms.llm("llama-3.1-tulu-3.1-8b")
+    result = model.respond("Напиши новости о природе в России")
+    bot.reply_to(message, result)
+
+
+
+
+bot.infinity_polling()
